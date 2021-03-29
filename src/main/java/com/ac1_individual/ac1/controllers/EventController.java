7a76fdbc3d382415e8d4505f677307d856d52c97
PPSasso <pp_sasso@hotmail.com>
2021-03-29 17:46:23 -0300

    Pagination and DB tests

--- src/main/java/com/ac1_individual/ac1/controllers/EventController.java
+++ src/main/java/com/ac1_individual/ac1/controllers/EventController.java
@@ -8,6 +8,9 @@ import com.ac1_individual.ac1.services.EventService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -53,7 +57,22 @@ public class EventController {
         Event event = service.getEventById(id);
 
         return ResponseEntity.ok().body(event);
-
     }
     
+    @GetMapping
+    public ResponseEntity<Page<Event>> getEvents(
+
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+        @RequestParam(value = "orderBy", defaultValue = "id") String orderBy
+
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+
+        Page<Event> events = service.getEvents(pageRequest);
+
+        return ResponseEntity.ok(events);
+
+    }
 }
--- src/main/java/com/ac1_individual/ac1/services/EventService.java
+++ src/main/java/com/ac1_individual/ac1/services/EventService.java
@@ -7,6 +7,8 @@ import com.ac1_individual.ac1.models.Event;
 import com.ac1_individual.ac1.repositories.EventRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,6 +23,12 @@ public class EventService {
         Event newEvent = repo.save(eventIn);
         
         return newEvent;
+    }
+
+    public Page<Event> getEvents(Pageable pageRequest){
+        Page<Event> pages = repo.find(pageRequest);
+
+        return pages;
     }
 
     public Event updateEvent(EventUpdateDTO eventIn, long id) {
